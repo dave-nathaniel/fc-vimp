24a58685d56983e99d34142c6d32b2defde60194
Dave Nathaniel <wajednathaniel@gmail.com>
2024-02-03 17:14:03 +0100

    Updated allowed hosts.

--- vimp/settings.py
+++ vimp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '20.101.63.100']
 
 AUTH_USER_MODEL = 'core_service.CustomUser'
 
