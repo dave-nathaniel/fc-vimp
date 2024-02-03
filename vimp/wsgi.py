9d0ad3ec432b3e48aa8855737669ed22d92c3c05
Dave Nathaniel <wajednathaniel@gmail.com>
2024-02-03 16:33:08 -0500

    Updated wsgi.py

--- vimp/wsgi.py
+++ vimp/wsgi.py
@@ -1,15 +1,16 @@
-"""
-WSGI config for vimp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(Path(__file__).resolve().parent.parent, '.env')
+load_dotenv(dotenv_path)
+
+# Add project directory to the sys.path
+path_home = str(Path(__file__).parents[1])
+if path_home not in sys.path:
+	sys.path.append(path_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vimp.settings')
 
