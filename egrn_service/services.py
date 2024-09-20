0d2e212c2bd0fb4a1e3ec434276f654e63d1f8e9
Dave Nathaniel <wajednathaniel@gmail.com>
2024-09-20 19:48:56 +0100

    Updated egrn_service.services.Middleware to use authentication.

--- egrn_service/services.py
+++ egrn_service/services.py
@@ -7,21 +7,51 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(Path(__file__).resolve().parent.parent, '.env')
 load_dotenv(dotenv_path)
 
-def get_store_from_middleware(*args, **kwargs):
+class Middleware:
 	'''
-		Retrieve store details from the middleware service.
-		Args:
-            kwargs (dict): Key-value pairs to be appended to the URL query parameters which identifies the store to retrieve.
+		Services provided by the middleware system.
 	'''
-	params = "&".join([f'{key}={value}' for key, value in kwargs.items()])
-	url = f'{os.getenv("MIDDLEWARE_HOST")}/api/v1/store'
-	url = f'{url}?{params}' if params else url
+	host = os.getenv('MIDDLEWARE_HOST')
+	user_id = os.getenv('MIDDLEWARE_USER')
+	password = os.getenv('MIDDLEWARE_PASS')
+	headers = {}
 	
-	try:
-		response = get(url)
+	def __init__(self):
+		self.authenticate()
+		
+	def authenticate(self):
+		'''
+		    Authenticate with the middleware service to obtain an access token.
+		'''
+		auth_url = f'{self.host}/api/v1/authenticate'
+		auth_data = {
+			"username": self.user_id,
+			"password": self.password
+		}
+		response = post(auth_url, json=auth_data)
 		if response.status_code == 200:
-			return response.json()['data']
-	except Exception as e:
-		logging.error(f"Error fetching store: {url}")
-	
-	return None
+			self.headers = {
+				'Authorization': f'Bearer {response.json().get("data",{}).get("access")}'
+			}
+		else:
+			logging.error(f"Authentication failed: {response.text}")
+
+	def get_store(self, *args, **kwargs):
+		'''
+			Retrieve store details from the middleware service.
+			Args:
+				kwargs (dict): Key-value pairs to be appended to the URL query parameters which identifies the store to retrieve.
+		'''
+		params = "&".join([f'{key}={value}' for key, value in kwargs.items()])
+		headers = self.headers
+		url = f'{os.getenv("MIDDLEWARE_HOST")}/api/v1/store'
+		url = f'{url}?{params}' if params else url
+		
+		try:
+			response = get(url, headers=headers)
+			if response.status_code == 200:
+				return response.json()['data']
+		except Exception as e:
+			logging.error(f"Error fetching store ({url}): {str(e)}")
+		
+		return None
