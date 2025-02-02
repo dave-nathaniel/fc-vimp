501fbeeb65f7e7879ab10fb1ae88c68d386a39e9
Dave Nathaniel <wajednathaniel@gmail.com>
2025-02-02 13:36:33 +0100

    Updated Admin view to exclude sensitive fields from CustomUser model.

--- core_service/admin.py
+++ core_service/admin.py
@@ -5,6 +5,23 @@ from .models import CustomUser, VendorProfile, TempUser
 class CustomUserAdmin(ModelAdmin):
 	# Search fields
 	search_fields = ['username', 'email', 'first_name', 'last_name']
+	exclude_from_view = ("secret", "password",)
+	
+	def get_fields(self, request, obj=None):
+		"""
+			Show 'secret' field only when creating a new user.
+		"""
+		fields = super().get_fields(request, obj)
+		return [field for field in fields if field not in self.exclude_from_view] if obj else fields
+
+	def get_readonly_fields(self, request, obj=None):
+		"""
+			Ensure 'secret' remains read-only even if accessed.
+		"""
+		if obj:  # If editing, make 'secret' readonly
+			return super().get_readonly_fields(request, obj) + self.exclude_from_view
+		return super().get_readonly_fields(request, obj)
+
 
 class VendorProfileAdmin(ModelAdmin):
 	# Search fields
