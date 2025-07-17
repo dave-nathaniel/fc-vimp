ea27d97903f617f7b92abc9dccbd396bbba6aeae
Dave Nathaniel <wajednathaniel@gmail.com>
2025-07-17 12:30:15 +0100

    Refactored ByD SOAP services implementation

--- byd_service/util.py
+++ byd_service/util.py
@@ -14,3 +14,11 @@ def format_datetime_to_iso8601(dt: datetime) -> str:
 		Convert a datetime object to an ISO 8601 string with UTC offset ('Z').
 	"""
 	return dt.strftime("%Y-%m-%dT%H:%M:%S")
+
+
+def ordinal(number):
+	if 10 <= number % 100 <= 20:
+		suffix = 'th'
+	else:
+		suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(number % 10, 'th')
+	return str(number) + suffix
