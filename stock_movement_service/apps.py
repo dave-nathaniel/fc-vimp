345265e7643d73e50bb935bd605f1f9b153879c7
Dave Nathaniel <wajednathaniel@gmail.com>
2025-08-06 11:23:32 +0100

    Removed redundant app.

--- stock_movement_service/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockMovementServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stock_movement_service'
-    verbose_name = "Stock Movement Service"
