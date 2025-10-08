a583a382f0ea5c25678227a85d8551c461e5b61f
prod-hotfix <wajednathaniel@gmail.com>
2025-10-08 18:32:46 +0000

    Fixed invoice quantity check.

--- invoice_service/models.py
+++ invoice_service/models.py
@@ -233,7 +233,7 @@ class InvoiceLineItem(models.Model):
 		return float(self.po_line_item.delivered_quantity) - invoiced
 		
 	def clean(self, ):
-		if self.quantity < 1:
+		if self.quantity < 0.00:
 			raise ValidationError("Invoice quantity must be greater than 0")
 		if float(self.quantity) > self.get_invoiceable_quantity():
 			raise ValidationError(f"Invoice quantity exceeds the outstanding invoiceable quantity ({self.get_invoiceable_quantity()})")
