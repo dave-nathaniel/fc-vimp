ca0eba15b8b3de854c5f235eae824f2d04eafa56
Gboyega <gboyega.banjo@gmail.com>
2026-03-24 09:37:08 +0100

    updated grn transfer build

---
+++ html/grn-transfer/assets/logo-DLrN504l.js
@@ -0,0 +1,1 @@
+const o="/grn-transfer/assets/logo-B5NoDtBU.png";export{o as F};
---
+++ html/grn-transfer/assets/useLocation-BRDSzPL0.js
@@ -0,0 +1,1 @@
+import{f as o}from"./index-BUS_ywuj.js";function s(e){return o({select:t=>t.location})}export{s as u};
